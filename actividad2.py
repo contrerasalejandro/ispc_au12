d6d3bf64b28a6c8df603656df6a1450c4226fe2f
Nievas2 <angelgabrielnievas@gmail.com>
2022-08-12 09:39:09 -0300

    actividad 1/2

---
+++ actividad2.py
@@ -0,0 +1,9 @@
+Dueno2=[23546987, "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+for i in range (0, 5):
+    if i == 0:
+        i = i + 1
+    elif i == 2:
+        i = i + 1
+    else:
+        print(Dueno2[i])
